10532380928636cae6291b6a7ad9a787598c7ff4
Juub0618 <dnqls1243@gmail.com>
2025-10-27 22:53:19 +0900

    feat: 스포너 시스템 리팩토링 및 리스폰 기능 추가
    Spawner<T> 추상 클래스 생성 (T : SpawnData)
    공통 스폰 로직 (랜덤 위치, 확률 계산 등) 재사용 가능
    아이템/몬스터 스포너 상속 사용
    
    리스폰 기능 추가
    DestructionTracker + IRespawnNotifier 인터페이스로 오브젝트 파괴 감지
    RespawnAfterDelay 코루틴으로 딜레이 후 재생성
    
    클래스 구조 개선
    변수, 메서드명 변경
    기존 딕셔너리 제거
    spawnedObjects로 생성 오브젝트와 데이터, 리스폰 관리
    OnObjectSpawnered 추상 메서드로 추가 초기화 가능

---
+++ Assets/0.Scripts/Map/Spawner/ItemSpawner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+//아이템 스포너
+public class ItemSpawner : Spawner<ItemSpawnData>
+{
+    protected override void OnObjectSpawnered(GameObject instance, ItemSpawnData spawnData)
+    {
+
+    }
+}
---
+++ Assets/0.Scripts/Map/Spawner/MonsterSpawner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+//몬스터 스포너
+public class MonsterSpawner : Spawner<MonsterData>
+{
+    protected override void OnObjectSpawnered(GameObject instance, MonsterData spawnData)
+    {
+
+    }
+}
