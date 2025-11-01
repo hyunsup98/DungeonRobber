e27a70da798b33aacf27e6f008a6a8eb2ffe5fa9
CommonMango <73099231+CommonMango@users.noreply.github.com>
2025-11-01 20:25:43 +0900

    Feat: BigBear추가
    1. 플레이어를 감지하지 못하면 다시 집으로 돌아오는 BigBear 로직 추가
    2. DetectAction후에 Overlook으로 돌아오지 못하는 문제점을 발경해 수정

--- Assets/0.Scripts/Entity/Monster/BigBear.cs
+++ Assets/0.Scripts/Entity/Monster/BigBear.cs
@@ -1,19 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.Android.Types;
-using Unity.VisualScripting;
-using Unity.VisualScripting.Antlr3.Runtime.Misc;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Animations;
 
-public class SkeletonWarrior : Monster
+
+public class BigBear : Monster
 {
     [SerializeField] WaitForSeconds detectDelay = new WaitForSeconds(1f);//감지 딜레이 
     [SerializeField] WaitForSeconds animeDelay = new WaitForSeconds(0.5f);//애니메이션 딜레이 
+    Vector3 homePosition;
+    bool isHome;//집인지 확인하는 변수
     int playerLayer;
-    int WaypointLayer;   
-    Coroutine detectPlayerCoroutine; //감지 코루틴  변수
+    Coroutine detectPlayerCoroutine; //감지 코루틴 변수
 
     private void Awake()
     {
@@ -23,12 +19,11 @@ public class SkeletonWarrior : Monster
     private void OnEnable() //활성화 시점에서 초기화 
     {
         Init();
-        StartCoroutine(WaitAnimationEnd("Spawn")); //스폰 애니메이션 종료까지 대기     
     }
     private void FixedUpdate()
     {
         agent.speed = stats.GetStat(StatType.MoveSpeed);
-
+        
         if (isDetectTarget) //감지했을 때
         {
             DetectAction();
@@ -42,20 +37,22 @@ public class SkeletonWarrior : Monster
     private void OnDisable() //비활성화시 코루틴 정지 
     {
         if (detectPlayerCoroutine != null)
-            StopCoroutine(detectPlayerCoroutine);
+        {
+            StopCoroutine(detectPlayerCoroutine);            
+        }
     }
 
     protected override void Init()
     {
         base.Init();
-        //attackRange = 2f;
-        //attackDelay = 1.5f;
+        //attackRange = 5f;
+        //attackDelay = 3f;
 
         detectPlayerCoroutine = StartCoroutine(nameof(DetectTarget));
-        //▼목적지 설정 
-        targetWaypoint = waypoints[Random.Range(0, waypoints.Count)];
-        
-        
+        //▼목적지 설정 플레이어 감지 전에는 집에서 안나가므로 현재 위치로 지정 
+        homePosition = transform.position;
+
+        isHome = true;
     }
     /// <summary>
     /// Awake시 초기화할 정보 담아놓은 메서드  
@@ -64,22 +61,20 @@ public class SkeletonWarrior : Monster
     {
         base.awakeinit();
         playerLayer = LayerMask.NameToLayer("Player");
-        WaypointLayer = LayerMask.NameToLayer("Waypoint");
     }
 
     protected override void Attack()
     {
         SetMoveBool(false); //이동 멈춤
-        monsterAnimator.SetTrigger("Attack");
+       
     }
     
-    public void AttackPlayer()
+    public void AttackEntity()
     {
        if(Vector3.SqrMagnitude(target.transform.position - transform.position) <= stats.GetStat(StatType.AttackRange) * stats.GetStat(StatType.AttackRange))
         {
             target.GetComponentInParent<Entity>().GetDamage(stats.GetStat(StatType.AttackDamage)); 
-        }
-                     
+        }            
     }
 
     /// <summary>
@@ -92,6 +87,7 @@ public class SkeletonWarrior : Monster
         targetDistance = Vector3.SqrMagnitude(tempVector); // 단순 비교이므로 sqrMagnitude 사용
         transform.LookAt(target.transform.position); //타겟 바라보기
         bool isPlayerinhit = false;
+        isHome = false;
 
         if (targetDistance <= stats.GetStat(StatType.AttackRange) * stats.GetStat(StatType.AttackRange)) //공격 사거리안에 들어오면 공격시작 
         {
@@ -126,12 +122,12 @@ public class SkeletonWarrior : Monster
     /// <param name="damage"> 해당 몬스터가 입을 피해 </param>
     public override void GetDamage(float damage)
     {
-        monsterAnimator.SetTrigger("GetDamage");
+        
         stats.ModifyStat(StatType.HP, -damage);
 
         if (stats.GetStat(StatType.HP) <= 0)
         {
-            monsterAnimator.SetTrigger("Die");
+            
             Destroy(this);
         }
     }
@@ -141,28 +137,19 @@ public class SkeletonWarrior : Monster
     /// </summary>
     protected override void OverlookAction()
     {
-       
-        SetMoveBool(true); //이동 상태로 전환
-        agent.SetDestination(targetWaypoint.position);//목적지로 이동  
-        
-        if (Vector3.SqrMagnitude(transform.position - targetWaypoint.position) < 10f) //a목적지에 주변에 도달했을 때
+        if (!isHome)
         {
-            SetMoveBool(false); //일단 멈춤
-            previousWaypoint = targetWaypoint; //이전 목적지에 현재 목적지 저장                        
+            SetMoveBool(true); //이동 상태로 전환
+            agent.SetDestination(homePosition);//목적지로 이동  
 
-            while (true && isDetectTarget == false)
+            if (Vector3.SqrMagnitude(transform.position - homePosition) < 1f) //a목적지에 주변에 도달했을 때
             {
-                Debug.Log(waypoints.Count);            
-                if (waypoints.Count <= 1) //목적지가 하나밖에 없으면
-                    break;
-
-                targetWaypoint = waypoints[Random.Range(0, waypoints.Count)];  //새 목적지 설정
-
-                if (targetWaypoint != previousWaypoint) //이전 목적지와 다르면
-                    break;
+                SetMoveBool(false); //일단 멈춤 
+                isHome = true;
             }
-           
+            
         }
+        
     }
 
     /// <summary>
@@ -172,7 +159,7 @@ public class SkeletonWarrior : Monster
     void SetMoveBool(bool toSetBool)
     {
         agent.isStopped = !toSetBool;
-        monsterAnimator.SetBool("isWalk", toSetBool);
+        
     }
 
 
@@ -183,13 +170,14 @@ public class SkeletonWarrior : Monster
     /// <returns></returns>
     protected override IEnumerator DetectTarget()
     {
-        
         bool foundTarget;
+        
         while (true)
         {
+            
+            foundTarget = false;       
             waypoints.Clear();
             colliders = Physics.OverlapSphere(transform.position, detectDestinationRadius, detectLayer);
-            foundTarget = false;
 
             if (colliders.Length > 0)
             {
@@ -197,21 +185,18 @@ public class SkeletonWarrior : Monster
                 {
                     //▼ 레이어가 player고 플레이어 감지거리 내에 있다면 
                     if (collider.gameObject.layer == playerLayer && Vector3.SqrMagnitude(collider.transform.position - transform.position) < playerDetectedRange * playerDetectedRange)
-                    {
+                    {                
                         target = collider.gameObject; //타겟 설정
                         foundTarget = true;
-                        isDetectTarget = true;
+                        isDetectTarget = true;                    
                         break;
+
                     }
-                    else if (collider.gameObject.layer == WaypointLayer)//레이어가 웨이포인트라면
-                    {
-                        waypoints.Add(collider.transform);
-                    }
-                }              
-                if (!foundTarget)
-                {
-                    isDetectTarget = false;
                 }
+                if(!foundTarget)
+                {                    
+                    isDetectTarget = false;                   
+                }               
             }
             else
             {
